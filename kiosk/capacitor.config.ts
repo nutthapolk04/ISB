25493fa0571092a65400e6fa4d9992190220d472
ilSPRITEli <spkittipat.ai@gmail.com>
2026-06-29 11:59:35 +0700

    chore: update dependencies for Capacitor and related packages
    - Added @capacitor/android, @capacitor/core, and @capacitor/cli with version 8.4.1 to both bun.lock and package.json.
    - Ensured compatibility with the latest Capacitor features and improvements.

---
+++ kiosk/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.okontek.app',
+    appName: 'Kiosk Balance App',
+    webDir: 'dist'
+};
+
+export default config;
+
